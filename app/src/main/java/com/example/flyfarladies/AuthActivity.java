c7f9a7542b5a7509975f7fa284e08dc8dd469337
Kayes Fahim <kayes.fuad@northsouth.edu>
2021-10-20 03:53:00 +0600

    Login Page Done

--- app/src/main/java/com/example/flyfarladies/AuthActivity.java
+++ app/src/main/java/com/example/flyfarladies/AuthActivity.java
@@ -2,7 +2,9 @@ package com.example.flyfarladies;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AuthActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class AuthActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+    }
+
+    public void AuthRegistration(View view) {
+        startActivity(new Intent(this, RegistrationActivity.class));
+    }
+
+    public void AuthLogin(View view) {
+        startActivity(new Intent(this, LoginActivity.class));
     }
 }
