dbd2d6f2ed8c574fecd6dc7f1c9b6c76cb529cdf
Kayes Fahim <kayes.fuad@northsouth.edu>
2021-10-21 10:46:03 +0600

    Edit Profile, Change Password

--- app/src/main/java/com/example/flyfarladies/ProfileActivity.java
+++ app/src/main/java/com/example/flyfarladies/ProfileActivity.java
@@ -1,8 +1,12 @@
 package com.example.flyfarladies;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import com.example.flyfarladies.Dashboard.ChangePasswordActivity;
+import com.example.flyfarladies.Dashboard.EditProfileActivity;
+import com.example.flyfarladies.Dashboard.SettingsActivity;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -11,5 +15,18 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         
+    }
+
+    public void ChangePassword(View view) {
+        startActivity(new Intent(this, ChangePasswordActivity.class));
+    }
+
+    public void EditProfile(View view) {
+        startActivity(new Intent(this, EditProfileActivity.class));
+
+    }
+
+    public void Settings(View view) {
+        startActivity(new Intent(this, SettingsActivity.class));
     }
 }
