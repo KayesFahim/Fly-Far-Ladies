2557fc8a4d771f0a1ed9583ad54cca1dbc542e14
Md Kayes Hossan Fuad <kayes.fuad@northsouth.edu>
2021-11-04 18:33:48 +0600

    ok

---
+++ app/src/main/java/com/example/flyfarladies/Packages/PackagesMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.flyfarladies.Packages;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.flyfarladies.R;
+import android.os.Bundle;
+
+public class PackagesMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packages_main);
+    }
+}
